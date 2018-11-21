1a7e06400a895fc409d6937e0b308ea08fcb4a9a
karthick <karthiram165@gmail.com>
2018-11-21 12:04:02 +0530

    pushes

---
+++ app.js
@@ -0,0 +1,30 @@
+// O(n)
+alert('hello');
+
+// as the n grows the runtime also grows
+
+function logatleattwo(n){
+
+    for(var i = 1; i <= Math.max(2, n); i++){
+
+        return i;
+
+    }
+
+}
+
+// O(1)
+
+// whatever may be the value of n.. the runtime stays constant
+
+function logatmostsix(n){
+
+    for(var i = 1; i <= Math.min(6, n); i++){
+
+        return i;
+
+    }
+
+}
+
+
